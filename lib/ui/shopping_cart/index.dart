dd6d8676586b23563a2aa61f1bad59a9adf6abf8
ManEnfu <manenfu@protonmail.com>
2022-08-01 14:33:35 +0700

    ListView

--- lib/ui/shopping_cart/index.dart
+++ lib/ui/shopping_cart/index.dart
@@ -8,6 +8,11 @@ class ShoppingCartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> items = [
+      'Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5',
+      'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10',
+    ];
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -17,7 +22,16 @@ class ShoppingCartPage extends StatelessWidget {
         ),
         title: const Text('Shopping Cart')
       ),
-      body: null,
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.teal.shade200,
+            padding: const EdgeInsets.all(40.0),
+            child: Text(items[index]),
+          );
+        },
+      ),
     );
   }
 }
